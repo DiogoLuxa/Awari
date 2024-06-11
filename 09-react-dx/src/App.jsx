806f8f8de3c991d0bb436ce3d02f6d766b904878
DiogoLuxa <diogoluxa.dev@gmail.com>
2024-06-11 10:12:48 -0300

    novo: adicionar projeto pasta 09-react-dx

---
+++ 09-react-dx/src/App.jsx
@@ -0,0 +1,23 @@
+// router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// components
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Error from "./pages/Error";
+
+function App() {
+  return (
+    <div className="max-w-screen-md m-auto">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detalhes/:id" element={<Details />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ 09-react-dx/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold mb-6">Assistentes Virtuais</h1>
+      <div className="space-x-4">
+        <Link to="/detalhes/siri" className="text-blue-500 hover:underline">
+          Siri
+        </Link>
+        <Link to="/detalhes/alexa" className="text-blue-500 hover:underline">
+          Alexa
+        </Link>
+        <Link to="/detalhes/google" className="text-blue-500 hover:underline">
+          Google Assistente
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
